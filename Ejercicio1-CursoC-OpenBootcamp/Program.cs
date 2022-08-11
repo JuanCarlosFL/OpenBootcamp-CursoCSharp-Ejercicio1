65b9cf845efbc87146294dacf92a94e8afda27de
Juan Carlos <jcarlos@fuentesdev.es>
2022-08-11 17:15:52 +0200

    Add project files.

---
+++ Ejercicio1-CursoC-OpenBootcamp/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Introduce tu nombre: ");
+var nombre = Console.ReadLine();
+Console.WriteLine($"Hola {nombre}");
+var hora = DateTime.Now;
+Console.WriteLine($"Son las {hora.Hour}:{hora.Minute} del día {hora.ToShortDateString()}");
